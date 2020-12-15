043f846a1e2c1fdb72e9294eea6ab3ba2124e5b2
jamesscaggs <james.scaggs@gmail.com>
2020-12-15 15:55:45 -0600

    needed to return an object with an array of objects to make this work

--- zapier.js
+++ zapier.js
@@ -2,14 +2,26 @@
 // you can use await throughout the function
 
 async function testing(inputData) {
-  const { skus } = inputData;
+  const {
+    skus,
+    potentialId,
+    company,
+    name,
+    phone,
+    street1,
+    city,
+    postalCode,
+    state,
+    customerEmail,
+    orderDate,
+    internalNotes,
+  } = inputData;
 
-  console.log(skus);
-  const productTitles = {
+  const products = {
     "sample-cln-acne-cleanser-aqua": "[Samples] Acne Cleanser 1 fl oz",
     "sample-cln-shampoo-red": "[Samples] Shampoo 1 fl oz",
     "sample-cln-gentle-shampoo-orange": "sample-cln-gentle-shampoo-orange",
-    "sample-cln-sportwash-blue": "[Samples] SportWash 1 fl oz ",
+    "sample-cln-sportwash-blue": "[Samples] SportWash 1 fl oz",
     "sample-cln-bodywash-green": "[Samples] BodyWash 1 fl oz",
     "sample-cln-handwash-magenta": "[Samples] HandWash 1 fl oz",
     "sample-cln-facial-cleanser-teal": "[Samples] Facial Cleanser 1 fl oz",
@@ -19,26 +31,13 @@ async function testing(inputData) {
   //create array of skus
   const skuArray = skus.split(",");
 
-  console.log(skuArray);
-
-  let output = [];
+  let items = [];
 
   await skuArray.forEach((sku) => {
-    let name;
-
-    // find title
-    for (const [key, value] in productTitles) {
-      if (sku === key) name = value;
-    }
-
-    output.push({ sku, name, quantity: 1, unitPrice: 0.0 });
+    let name = products[sku];
+    items.push({ sku, name, quantity: 1, price: 0.0 });
   });
 
-  console.log({ output });
-
-  return output;
+  // you have to return an object. If you return an object with an array of objects zapier will execute the following step multiple times.
+  return { items };
 }
-
-testing(
-  "sample-cln-acne-cleanser-aqua,sample-cln-acne-cleanser-aqua,sample-cln-facial-cleanser-teal"
-);
